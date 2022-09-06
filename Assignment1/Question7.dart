f806fd5ec29206f75f9490129c0940d0cc412766
nandhuraj007 <112924887+nandhuraj007@users.noreply.github.com>
2022-09-06 11:44:29 +0530

    Add files via upload

---
+++ Assignment1/Question7.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+void main(){
+  print("enter the limit to print even numbers");
+  int number=int.parse(stdin.readLineSync()!);
+  for(int i=2;i<number;i++){
+    if(i%2==0){
+      print(i);
+    }
+    }
+}
+
